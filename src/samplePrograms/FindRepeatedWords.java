5a33a8bb5cdc573bc165cf796316b8e347e675ca
pb21071 <pb21071@OJAS-DD402.corp.ojas-it.com>
2021-03-29 16:45:16 +0530

    Added

--- src/samplePrograms/FindRepeatedWords.java
+++ src/samplePrograms/FindRepeatedWords.java
@@ -26,12 +26,12 @@ public class FindRepeatedWords {
 	        list.add("java");
 	        
 	        
-//	       list.stream().filter(s -> s.length() > 5).skip(2).forEach(System.out::println);
-//	       System.out.println();
-//	       
-//	       list.stream().map(String::toUpperCase).forEach(System.out::println);
-//	       System.out.println(); 
-//	       list.forEach(System.out::println);
+	       list.stream().filter(s -> s.length() > 5).skip(2).forEach(System.out::println);
+	       System.out.println();
+	       
+	       list.stream().map(String::toUpperCase).forEach(System.out::println);
+	       System.out.println(); 
+	       list.forEach(System.out::println);
 	       System.out.println();
 	       list.stream().sorted((s1, s2) -> s1.length() - s2.length()).skip(2).forEach(System.out::println);
 	       
